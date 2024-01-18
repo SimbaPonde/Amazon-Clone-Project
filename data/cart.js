ed2ae9a66eb57740ca204b93c6004043f604634e
Forza <forza@nyajena.com>
2024-01-18 14:20:09 +0200

    Delete product from cart when delete is clicked

--- data/cart.js
+++ data/cart.js
@@ -1,9 +1,9 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
   },
-  
+
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
@@ -28,4 +28,15 @@ export function addToCart(productId) {
           });
         }
   
+  }
+
+  export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach((cartItem)=>{
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    })
+
+    cart = newCart;
   }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -41,7 +41,7 @@ cartSummaryHTML+= `
     <span class="update-quantity-link link-primary">
         Update
     </span>
-    <span class="delete-quantity-link link-primary">
+    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
         Delete
     </span>
     </div>
@@ -98,3 +98,11 @@ cartSummaryHTML+= `
 })
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) =>{
+    link.addEventListener('click', () =>{
+       const productId = link.dataset.productId;
+       removeFromCart(productId);
+    });
+}
+)
