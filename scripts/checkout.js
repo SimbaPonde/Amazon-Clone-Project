7500562dcb591113ddef555fafa7aa60cfadd773
Forza <forza@nyajena.com>
2024-01-18 14:39:50 +0200

    Delete Products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem)=> {
     })
 
 cartSummaryHTML+= `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+   js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
      Delivery date: Tuesday, June 21
     </div>
@@ -103,6 +104,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>{
     link.addEventListener('click', () =>{
        const productId = link.dataset.productId;
        removeFromCart(productId);
+
+       const container = document.querySelector(`.js-cart-item-container-${productId}`);
+       container.remove();
     });
+
 }
 )
