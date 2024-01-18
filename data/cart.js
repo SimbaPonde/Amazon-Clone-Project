111483a423ae465ff048d7169d81e67434dba3cf
Forza <forza@nyajena.com>
2024-01-18 14:55:29 +0200

    Save cart in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
-export let cart = [
-  {
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+ if (!cart) {
+
+  cart = [
+  { 
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
   },
@@ -7,8 +11,12 @@ export let cart = [
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
-  }
-];
+  }];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -27,7 +35,7 @@ export function addToCart(productId) {
             quantity: 1
           });
         }
-  
+  saveToStorage();
   }
 
   export function removeFromCart(productId) {
@@ -39,4 +47,5 @@ export function addToCart(productId) {
     })
 
     cart = newCart;
+    saveToStorage();
   }
