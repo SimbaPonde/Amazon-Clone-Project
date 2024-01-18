7c58e95aedd46d18c02226ac0ed25a827b354686
Forza <forza@nyajena.com>
2024-01-18 13:02:41 +0200

    Fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML= '';
 
@@ -31,7 +32,7 @@ cartSummaryHTML+= `
       ${matchingProduct.name}
     </div>
     <div class="product-price">
-    $${matchingProduct.priceCents / 100}
+    $${formatCurrency(matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
     <span>
@@ -53,7 +54,7 @@ cartSummaryHTML+= `
     <div class="delivery-option">
     <input type="radio" checked
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
     <div>
         <div class="delivery-option-date">
         Tuesday, June 21
@@ -66,7 +67,7 @@ cartSummaryHTML+= `
     <div class="delivery-option">
     <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
     <div>
         <div class="delivery-option-date">
         Wednesday, June 15
@@ -79,7 +80,7 @@ cartSummaryHTML+= `
     <div class="delivery-option">
     <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
     <div>
         <div class="delivery-option-date">
         Monday, June 13
@@ -97,4 +98,3 @@ cartSummaryHTML+= `
 })
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
-console.log(cartSummaryHTML);
