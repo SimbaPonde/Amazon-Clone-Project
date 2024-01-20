3d0ea0943cdcc3840c40d8e143a6e5c1b2e32a64
Forza <forza@nyajena.com>
2024-01-20 15:58:19 +0200

    save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js'
-import { products } from '../../data/products.js';
+import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 
 
 
@@ -20,24 +20,11 @@ export function renderOrderSummary(){
 
         const productId = cartItem.productId;
 
-        let matchingProduct;
-    
-        products.forEach((product) => {
-            if (product.id === productId){
-                matchingProduct=product;
+        const matchingProduct = getProduct(productId);
 
-            }
-        })
-    
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-        }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(
