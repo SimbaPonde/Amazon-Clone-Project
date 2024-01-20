5e5d29d6bfe08e7438e465fe94726389498f9bb8
Forza <forza@nyajena.com>
2024-01-20 16:06:51 +0200

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,17 +3,11 @@ import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 
 export function renderOrderSummary(){
-
-
-
-    const today = dayjs();
-    const deliveryDate= today.add(7, 'days');
-    deliveryDate.format('dddd, MMMM, D');
-
     let cartSummaryHTML= '';
 
     cart.forEach((cartItem)=> {
@@ -124,6 +118,8 @@ export function renderOrderSummary(){
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+
+        renderPaymentSummary();
         });
 
     }
@@ -134,6 +130,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId}= element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
         })
     })
 }
